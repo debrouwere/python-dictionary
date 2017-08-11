d43e879329600bc52d109d68180c63fd285c494f
Stijn Debrouwere <stijn@debrouwere.org>
2017-08-11 20:49:02 +0200

    Bump version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 from setuptools import find_packages, setup
 
 setup(name='dictionary-helpers',
-      version='0.1.0',
+      version='0.1.1',
       description=("Utility functions for working with dictionaries."),
       #long_description=open('README.rst').read(),
       classifiers=['Development Status :: 3 - Alpha',
