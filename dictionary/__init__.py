66c66f72d57ce99e194ec7d06b45ba6ff2e429f6
Stijn Debrouwere <stijn@debrouwere.org>
2017-08-11 20:48:49 +0200

    Add all functions to exports.

--- dictionary/__init__.py
+++ dictionary/__init__.py
@@ -5,7 +5,7 @@ from .navigate import keys, values, find, traverse, pluck, predicate
 from .reshape import flip, inflate, deflate, items, columns, records
 from .match import matches, equals
 from .serialize import simplify
-from .transform import defaults, transform, rekey, revalue, dictionary, forwards, backwards, merge, groupby, indexby, humanize, slugify, snakify
+from .transform import whitelist, blacklist, pick, omit, defaults, transform, rekey, revalue, dictionary, forwards, backwards, merge, groupby, indexby, humanize, slugify, snakify
 from .types import order, sort, blob, namedtuple, options
 
 from copy import copy, deepcopy
